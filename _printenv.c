86e677ce63225e956dfda12d748173b18f22ebba
Felipe <felipe_londono1@hotmail.com>
2019-11-27 18:16:29 +0000

    [FIX]: betty style

--- _printenv.c
+++ _printenv.c
@@ -1,17 +1,18 @@
 #include "shell.h"
-
-extern char **environ;
-
+/**
+ * _printenv - print enviroment
+ *
+ */
 void _printenv(void)
 {
-    char **ep;
-    int i = 0;
+	char **ep;
+	int i = 0;
 
-    ep = environ;
-    while (ep[i] != NULL)
-    {
-        write(1, ep[i], _strlen(ep[i]));
-        write(1, "\n", 1);
-        i++;
-    }
+	ep = environ;
+	while (ep[i] != NULL)
+	{
+		write(1, ep[i], _strlen(ep[i]));
+		write(1, "\n", 1);
+		i++;
+		}
 }
